7f19b3508b02e578b67268e5ef26f9a035bedaa2
Cherie Yang <cherie@cmu.edu>
2015-06-08 14:56:26 -0400

    Fixed style issues

--- cl-mci.lisp
+++ cl-mci.lisp
@@ -12,24 +12,24 @@
 
 (defparameter *song* nil)
 
+; Helper function that calls mcisendstringa
+(defun mci-send-string (command) 
+  (mcisendstringa 
+   (concatenate 'string command " " *song*) 
+   "" 0 0))
+
 ; Use this to set the current song. 
 ; song needs to be something.mp3
 (defun set-song (song) 
-  (defparameter *song* song))
+  (setf *song* song))
 
 ; play-song is a helper function. 
 (defun play-song () 
   (let ((status 
-	 (mcisendstringa 
-	  (concatenate 'string 
-		       "open " *song* " alias song")
-	  "" 0 0)))
+	 (mci-send-string "open")))
     (if (= status 0) 
 	1
-	(mcisendstringa 
-	 (concatenate 'string 
-		      "play " *song* "")
-	 "" 0 0))))
+	(mci-send-string "play"))))
 
 ; Plays the current song. 
 ; It will keep trying to play it if there is an error, 
@@ -38,25 +38,20 @@
 (defun play () 
   (if *song* 
       (if (= (play-song) 0)
-	  0 (play)) 
+	  0 
+	  (play)) 
       "Error: You must first specify a song with SET-SONG."))
 
 ; Pauses the current song. 
 (defun pause () 
-  (mcisendstringa 
-   (concatenate 'string "pause " *song*)
-   "" 0 0))
+  (mci-send-string "pause"))
 
 ; Stops the current song. 
 (defun stop () 
-  (mcisendstringa 
-   (concatenate 'string "stop " *song*)
-   "" 0 0))
+  (mci-send-string "stop"))
 
 ; Please remember to call this when you're done with a song! 
 (defun close-song () 
   (if *song* 
-      (mcisendstringa 
-       (concatenate 'string "close " *song*)
-       "" 0 0)
+      (mci-send-string "close")
       nil))
